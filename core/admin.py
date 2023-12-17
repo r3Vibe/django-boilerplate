83fed86654bb73141cb2bdf830e8d6273902e6a3
arnabg95 <arnab.gupta@weavers-web.com>
2023-12-17 20:46:17 +0530

    script update

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,77 @@
-from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as UserAdminBase
 
-# Register your models here.
+@register(get_user_model())
+class UserAdmin(UserAdminBase):
+
+    def get_queryset(self, request):
+        """do not show the superuser account on the users list"""
+        qs = super().get_queryset(request)
+        return qs.exclude(is_superuser=True)
+    
+    list_display = ['first_name','last_name','email','is_active','is_staff']
+    list_editable = ['is_active','is_staff']
+    readonly_fields = ['date_created','date_modified']
+    list_display_links = ['first_name','last_name','email']
+    list_filter= ['is_active','is_staff']
+    search_fields = ['first_name','last_name','email']
+    fieldsets = (
+        ("Personal Info", {
+            "fields": (
+                'first_name',
+                'last_name'
+            ),
+        }),
+        ("Contact Info", {
+            "fields": (
+                'email',
+            ),
+        }),
+        ("Privilages", {
+            "fields": (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups'
+            ),
+        }),
+        ("Password", {
+            "fields": (
+                'password',
+            ),
+        }),
+        ("Other Info", {
+            "fields": (
+                'date_created',
+                'date_modified',
+            ),
+        }),
+    )
+    add_fieldsets  = (
+        ("Personal Info", {
+            "fields": (
+                'first_name',
+                'last_name'
+            ),
+        }),
+        ("Contact Info", {
+            "fields": (
+                'email',
+            ),
+        }),
+        ("Privilages", {
+            "fields": (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+        ("Password", {
+            "fields": (
+                'password1',
+                'password2',
+            ),
+        }),
+    )
+    ordering = ['email']
--- core/models.py
+++ core/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """custom user model manager"""
+    def create_user(self, email, password,**extras):
+        """create new user and return it"""
+        if not email:
+            raise ValueError("Email is required")
+        
+        if not password:
+            raise ValueError("Password is required")
+        
+        user = self.model(email=self.normalize_email(email), **extras)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, password, **extras):
+        """create superuser"""
+        user = self.create_user(email, password, **extras)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """custom user model for the project"""
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name']
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.email
